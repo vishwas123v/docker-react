6455f97c58a6cdbc60679c4c3b10e40baf96fc98
vishwas123v <vishwas123v@gmail.com>
2020-07-16 10:10:41 +0530

    msg changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Bye Vishwas
+          Edit <code>src/App.js</code> I was changed on feature branch
         </p>
         <a
           className="App-link"
